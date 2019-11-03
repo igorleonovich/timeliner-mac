b0a9d9a22fe457a073c5b66005e0c1e00542157d
Igor Leonovich <ceo@signaldance.com>
2019-11-03 16:59:16 +0300

    Add aperture recording

--- Timeliner/RecordingManager.swift
+++ Timeliner/RecordingManager.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import Aperture
 
 enum RecordingState {
     case idle
@@ -15,12 +16,24 @@ enum RecordingState {
 class RecordingManager {
     
     var state = RecordingState.idle
+    var aperture: Aperture
+    
+    init() {
+        do {
+            let fileURL = URL(fileURLWithPath: "rec.mp4")
+            aperture = try Aperture.init(destination: fileURL, framesPerSecond: 24, cropRect: nil, showCursor: true, highlightClicks: false)
+        } catch {
+            fatalError("Cannot initialize Aperture")
+        }
+    }
     
     func startRecording() {
         state = .recording
+        aperture.start()
     }
     
     func stopRecording() {
         state = .idle
+        aperture.stop()
     }
 }
